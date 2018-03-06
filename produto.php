bd50ac7da41526a742c570dc97fc5bb124705e92
Priscilla De Souza Valciano <priscilladesouzavalciano@Mac-mini-de-Priscilla.local>
2018-03-06 00:34:40 -0300

    adaptando a pag produto para desktop

--- produto.php
+++ produto.php
@@ -2,32 +2,47 @@
         $cabecalho_title = "Produto da Mirror Fashion";
         include("cabecalho.php");
     ?>
-    <div class="produto">
-        <h1>Fuzzy Cardigan</h1>
-        <p>por apenas R$ 129,00</p>
+    <div class="container">
+        <div class="produto">
+            <h1>Fuzzy Cardigan</h1>
+            <p>por apenas R$ 129,00</p>
 
-        <form>
-            <fieldset class="cores">
-                <legend>Escolha a cor:</legend>
+            <form>
+                <fieldset class="cores">
+                    <legend>Escolha a cor:</legend>
 
-                <input type="radio" name="cor" value="verde" id="verde" checked>
-                <label for="verde">
-                    <img src="img/produtos/foto2-verde.png" alt="verde">
-                </label>
+                    <input type="radio" name="cor" value="verde" id="verde" checked>
+                    <label for="verde">
+                        <img src="img/produtos/foto2-verde.png" alt="verde">
+                    </label>
 
-                <input type="radio" name="cor" value="rosa" id="rosa" checked>
-                <label for="rosa">
-                    <img src="img/produtos/foto2-rosa.png" alt="rosa">
-                </label>
+                    <input type="radio" name="cor" value="rosa" id="rosa" checked>
+                    <label for="rosa">
+                        <img src="img/produtos/foto2-rosa.png" alt="rosa">
+                    </label>
 
-                <input type="radio" name="cor" value="azul" id="azul" checked>
-                <label for="azul">
-                    <img src="img/produtos/foto2-azul.png" alt="azul">
-                </label>
-            </fieldset>
+                    <input type="radio" name="cor" value="azul" id="azul" checked>
+                    <label for="azul">
+                        <img src="img/produtos/foto2-azul.png" alt="azul">
+                    </label>
+                </fieldset>
+                <fieldset class="tamanhos">
+                    <legend>Escolha o tamanho:</legend>
+                    <input type="range" min="36" max="46" value="42" step="2" name="tamanho" id="tamanho">
+                </fieldset>
 
-            <input type="subimit" class="comprar" value="Comprar">
-        </form>
+                <input type="submit" class="comprar" value="Comprar">
+            </form>
+        </div>
+        <div class="detalhes">
+            <h2>Detalhes do produto</h2>
+            <p>
+                Esse é o melhor casaco de Cardigã que você já viu. Excelente
+                material italiano com estampa desenhada pelos artesãos da 
+                comunidade de Krotor nas ilhas gregas. Compre já e receba hoje
+                mesmo pela nossa entrega a jato.
+            </p>
+            
     </div>
 
 
